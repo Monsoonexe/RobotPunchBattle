e600f9ad79d4a126857604bfe2f0f908931e1628
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-05-14 14:56:35 -0700

    deleted old, unused files

--- Robot Fight/Robot Battle V5_12/testing module.py
+++
@@ -1,9 +0,0 @@
-#testing module
-from robotClass import *
-from itemmodule import *
-from robot_battle_test import *
-playerBot, compBot = initializeRob() #build robots
-moveDic = moveDictionary() #build move dictionary
-itemDic = itemDictionary() #build item dictionary
-
-activateItem(itemDic["grenade"], playerBot, compBot)
